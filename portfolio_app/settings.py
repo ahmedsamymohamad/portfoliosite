6cad6e01b5b7eb4b25421f4afc3f2a162ea5d056
ahmedsamymohamad <116212699+ahmedsamymohamad@users.noreply.github.com>
2024-07-30 08:20:09 +0300

    Update settings.py

--- portfolio_app/settings.py
+++ portfolio_app/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v8bzoj5)*&_%x-yy7o*z-2$*m1uuo*hbtb(n)%@bboej@%wkox'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == True
-
+DEBUG = os.getenv('DEBUG') == True
+print(f"DEBUG MODE {DEBUG}")
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
