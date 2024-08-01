976d429c3b21ce55bd5da89fa8b1ab6da41d5dc9
ahmedsamymohamad <116212699+ahmedsamymohamad@users.noreply.github.com>
2024-08-02 01:22:55 +0300

    Update settings.py

--- portfolio_app/settings.py
+++ portfolio_app/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = 'django-insecure-v8bzoj5)*&_%x-yy7o*z-2$*m1uuo*hbtb(n)%@bboej@%wkox
 # SECURITY WARNING: don't run with debug turned on in production!
 
 ALLOWED_HOSTS = [
-    '*'
+    '127.0.0.1',
+    '.vercel.app'
 ]
 
 print(f"ALLOWED_HOSTS {ALLOWED_HOSTS}")
